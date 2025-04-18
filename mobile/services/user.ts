9688d8fdd9871edd9be0aa81458121e2b7a03c19
Alexis HELM <128365251+Lydovw@users.noreply.github.com>
2025-04-18 12:18:58 +1000

    services done

---
+++ mobile/services/user.ts
@@ -0,0 +1,21 @@
+import { AuthResponse } from "@/types/user";
+import { Api } from "./api";
+
+type Credentials = {
+  email: string;
+  password: string;
+};
+
+async function login(credentials: Credentials): Promise<AuthResponse> {
+  return Api.post("/auth/login", credentials);
+}
+async function register(credentials: Credentials): Promise<AuthResponse> {
+  return Api.post("/auth/register", credentials);
+}
+
+const userService = {
+  login,
+  register,
+};
+
+export { userService };
